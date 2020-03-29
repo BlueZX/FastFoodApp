c50238051c0185458aad09287176a5ee5f0b35a2
icastro <icastro@integralit.cl>
2020-03-29 18:08:02 -0300

    add description

--- app/src/main/java/cl/bluez/fastfoodapp/Activity/SandwichActivity.java
+++ app/src/main/java/cl/bluez/fastfoodapp/Activity/SandwichActivity.java
@@ -39,9 +39,18 @@ public class SandwichActivity extends AppCompatActivity {
 
         Sandwich italiano = new Sandwich("Churrasco Italiano","Contiene Filetes delgados de carne lomo de vacuno, 4 Paltas, 4 Tomates, 4 Panes para Sándwich, 2 Dientes de ajo 1/2 Limón, Aceite de Oliva, Pimienta, Sal a gusto, Mayonesa, ketchup, mostaza a elección",3500,R.drawable.italiano);
         Sandwich chacarero = new Sandwich("Churrasco Chacarero", "CONTIENE 400 GRAMOS DE BISTEC DE POSTA ROSADA, POROTOS VERDES, TOMATE GRANDE, SAL, PIMIENTA  Y MANTEQUILLA QUE REPOSAN SOBRE DOS PANES FRICA",3700,R.drawable.chacarero);
-        Sandwich barrosluco = new Sandwich("Lomo italiano","bldsgfsjdlksfdf", 3000,R.drawable.lomo);
-        Sandwich churrasco = new Sandwich("Churrasco chacarero","bldsgfsjdlksfdf", 2900,R.drawable.churrasco);
-        Sandwich pollo = new Sandwich("Pollo con Albondigas","bldsgfsjdlksfdf", 2500,R.drawable.pollo_albonigas);
+        Sandwich barrosluco = new Sandwich("Lomo italiano","400 gramos de bistec de posta rosada a temperatura ambiente\n" +
+                "2 paltas grandecitas\n" +
+                "1 tomate grande y maduro\n" +
+                "1 pan frica\n" +
+                "mayonesa\n" +
+                "ají verde para servir", 3000,R.drawable.lomo);
+        Sandwich churrasco = new Sandwich("Churrasco chacarero"," 1 pan frica, \n" +
+                "1 bistec de posta, asiento\n" +
+                "porotos verdes (1 taza más o menos)\n" +
+                "1 tomate grande y bien maduro\n" +
+                "mantequilla", 2900,R.drawable.churrasco);
+        Sandwich pollo = new Sandwich("Pollo con Albondigas","Contiene Pan, Albondigas de pollo, queso cheddar, cebolla, pimienta roja y pesto", 2500,R.drawable.pollo_albonigas);
 
         ArrayList<Sandwich> data = new ArrayList<Sandwich>();
 
